1584b9b96babad01012f5fd50cf250c92b486e85
Minh Le <vietminhle98@gmail.com>
2023-04-04 09:24:51 +1000

    update script to inspect any url

--- src/scripts/inspect.ts
+++ src/scripts/inspect.ts
@@ -1,20 +1,31 @@
+import { AIPlugin } from "../ai_plugin";
 import { ManifestSchema } from "../manifest_schema";
 import { PluginExplorer } from "../plugin_explorer";
 import { RedirectValidator } from "../redirect_validator";
 
 const main = async () => {
+  const url = process.argv[2];
+  if (!url) {
+    throw new Error("A url must be provided");
+  }
+
   const explorer = new PluginExplorer(
     fetch,
     ManifestSchema,
     new RedirectValidator()
   );
-  const url = "https://www.klarna.com/";
 
   console.log("Provided url", url);
 
   const manifest = await explorer.inspect(url);
+  console.log("Fetched manifest", manifest?.name_for_human);
 
-  console.log("Fetched manifest", manifest);
+  if (!manifest) {
+    return;
+  }
+
+  const plugin = new AIPlugin(manifest);
+  console.log("Plugin", plugin);
 };
 
 main();
